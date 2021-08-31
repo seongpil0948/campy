d6fc3c57a5e421d803c0bd5dfc8042c26f3dfae1
seongpil0948 <seongpil0948@gmail.com>
2021-09-01 00:39:31 +0900

    firebase init widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
+import 'package:campy/views/pages/common/wrong.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
 import 'package:campy/views/router/delegate.dart';
 import 'package:campy/views/router/state.dart';
-import 'package:flutter/material.dart';
 
 void main() {
   runApp(PyApp());
@@ -9,13 +11,30 @@ void main() {
 class PyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Flutter Demo',
-        theme: ThemeData(
-          primarySwatch: Colors.blue,
-        ),
-        home: MaterialApp(
-          home: Router(routerDelegate: PyRouterDelegate(PyState())),
-        ));
+    return FutureBuilder(
+      future: Firebase.initializeApp(),
+      builder: (ctx, snapshot) {
+        if (snapshot.hasData) {
+                return MaterialApp(
+          title: 'Flutter Demo',
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+          ),
+          home: MaterialApp(
+            home: Router(routerDelegate: PyRouterDelegate(PyState())),
+          ));
+        }
+        else if (snapshot.hasError) {
+          print("=== Flutter Initialize Error ===");
+          return Wrong();
+        } else {
+          return CircularProgressIndicator();
+        }
+
+      },
+
+    );
   }
 }
+
+
