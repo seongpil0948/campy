53172e3b0bfa34fd2c5ecce487755d86d04f6ea0
seongpil0948 <seongpil0948@gmail.com>
2021-09-15 00:58:55 +0900

    serialize

--- lib/models/feed.dart
+++ lib/models/feed.dart
@@ -1,12 +1,13 @@
 import 'package:campy/models/user.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class FeedInfo {
   const FeedInfo({
     required this.writer,
     required this.isfavorite,
     required this.feedId,
-    required this.img,
+    required this.files,
     required this.content,
     required this.hashTags,
     required this.likeCount,
@@ -16,12 +17,37 @@ class FeedInfo {
   });
   final PyUser writer;
   final bool isfavorite;
-  final String feedId;
+  final String? feedId;
   final String hashTags;
-  final ImageProvider img;
+  final List<XFile> files;
   final String content;
   final int likeCount;
   final int commentCount;
   final int shareCount;
   final int bookmarkCount;
+
+  FeedInfo.fromJson(Map<String, dynamic> j)
+      : writer = PyUser.fromJson(j),
+        isfavorite = j['isfavorite'],
+        feedId = j['id'],
+        files = j['files'],
+        content = j['content'],
+        hashTags = j['hashTags'],
+        likeCount = j['likeCount'],
+        commentCount = j['commentCount'],
+        shareCount = j['shareCount'],
+        bookmarkCount = j['bookmarkCount'];
+
+  Map<String, dynamic> toJson() => {
+        'writer': writer.toJson(),
+        'isfavorite': isfavorite,
+        'feedId': feedId,
+        'files': files,
+        'content': content,
+        'hashTags': hashTags,
+        'likeCount': likeCount,
+        'commentCount': commentCount,
+        'shareCount': shareCount,
+        'bookmarkCount': bookmarkCount,
+      };
 }
--- lib/models/user.dart
+++ lib/models/user.dart
@@ -3,15 +3,63 @@ import 'package:campy/models/feed.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class PyUser {
-  final User socialUser;
-  String get profileImage => socialUser.photoURL ?? PyDefault.profileImage;
+  String displayName;
+  String email;
+  bool emailVerified;
+  String phoneNumber;
+  UserMetadata metadata;
+  String photoURL;
+  List<UserInfo> providerData;
+  String refreshToken;
+  String tenantId;
+  int hash;
+  String get profileImage => photoURL;
   List<FeedInfo> feeds = [];
   List<PyUser> followers = [];
   List<PyUser> follows = [];
-  PyUser({required this.socialUser});
+  PyUser({required User user})
+      : displayName = user.displayName!,
+        email = user.email!,
+        emailVerified = user.emailVerified!,
+        phoneNumber = user.phoneNumber!,
+        metadata = user.metadata!,
+        photoURL = user.photoURL!,
+        providerData = user.providerData,
+        refreshToken = user.refreshToken!,
+        tenantId = user.tenantId!,
+        hash = user.hashCode;
 
-  @override
-  String toString() {
-    return "PyUser: Social: $socialUser \n feeds: $feeds \n followers: $followers \n follows: $follows";
-  }
+  // @override
+  // String toString() {
+  //   return "PyUser: Social: $ \n feeds: $feeds \n followers: $followers \n follows: $follows";
+  // }
+
+  PyUser.fromJson(Map<String, dynamic> j)
+      : displayName = j['displayName'],
+        email = j['email'],
+        emailVerified = j['emailVerified'],
+        phoneNumber = j['phoneNumber'],
+        metadata = j['metadata'],
+        photoURL = j['photoURL'],
+        providerData = j['providerData'],
+        refreshToken = j['refreshToken'],
+        tenantId = j['tenantId'],
+        hash = j['hashCode'],
+        followers = j['followers'],
+        follows = j['follows'];
+
+  Map<String, dynamic> toJson() => {
+        'displayName': displayName,
+        'email': email,
+        'emailVerified': emailVerified,
+        'phoneNumber': phoneNumber,
+        'metadata': metadata,
+        'photoURL': photoURL,
+        'providerData': providerData,
+        'refreshToken': refreshToken,
+        'tenantId': tenantId,
+        'hashCode': hashCode,
+        'followers': followers,
+        'follows': follows,
+      };
 }
