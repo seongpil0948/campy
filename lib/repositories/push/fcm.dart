4cebf9698fec6b31585af66eca1f366b2331c6bb
seongpil0948 <seongpil0948@gmail.com>
2021-12-18 00:17:06 +0900

    안드로이드 다시 초기화

--- lib/repositories/push/fcm.dart
+++ lib/repositories/push/fcm.dart
@@ -1,50 +1,32 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
 
-// import 'package:firebase_messaging/firebase_messaging.dart';
+Future<void> fcmInitialize() async {
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
+  // returned from the request details information regarding the users decision.
+  NotificationSettings settings = await messaging.requestPermission(
+    alert: true,
+    announcement: false,
+    badge: true,
+    carPlay: false,
+    criticalAlert: false,
+    provisional: false,
+    sound: true,
+  );
+  print(
+      'FirebaseMessaging User granted permission: ${settings.authorizationStatus}');
 
-// Future<void> fcmInitialize () async {
-//   /// Update the iOS foreground notification presentation options to allow
-//   /// heads up notifications.
-//   await FirebaseMessaging
-//       .setForegroundNotificationPresentationOptions(
-//     alert: true,
-//     badge: true,
-//     sound: true,
-//   );  
-//   NotificationSettings settings = await messaging.requestPermission(
-//     alert: true,
-//     announcement: false,
-//     badge: true,
-//     carPlay: false,
-//     criticalAlert: false,
-//     provisional: false,
-//     sound: true,
-//   );
+  FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+    print('Got a message whilst in the foreground!');
+    print('Message data: ${message.data}');
 
-// }
+    if (message.notification != null) {
+      print('Message also contained a notification: ${message.notification}');
+    }
+  });
 
+  FirebaseMessaging.onBackgroundMessage(_firebaseMessagingBackgroundHandler);
+}
 
-// class PyFcm {
-//   FirebaseMessaging inst;
-//   // === Singleton ===
-//   PyFcm._onlyOne(): 
-//     inst = FirebaseMessaging.instance;
-//   static final PyFcm _instance = PyFcm._onlyOne();
-//   factory PyFcm() {
-//     return _instance;
-//   }  
-// }
-
-// FirebaseMessaging messaging = FirebaseMessaging.instance;
-
-
-
-// print('User granted permission: ${settings.authorizationStatus}');
-
-// FirebaseMessaging.onMessage.listen((RemoteMessage message) {
-//   print('Got a message whilst in the foreground!');
-//   print('Message data: ${message.data}');
-
-//   if (message.notification != null) {
-//     print('Message also contained a notification: ${message.notification}');
-//   }
-// });
+Future<void> _firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  print("Handling a background message: ${message.toString()}");
+}
--- scripts/gen_android_pub_certi.sh
+++ scripts/gen_android_pub_certi.sh
@@ -9,5 +9,4 @@
 # 비공개 키
 # java -jar pepk.jar --keystore=/Users/sp/Codes/Camps/campy_secret/upload-keystore.jks --alias=CampyAndroidKey --output=output.zip --include-cert --encryptionkey=eb10fe8f7c7c9df715022017b00c6471f8ba8170b13049a11e6c09ffe3056a104a3bbe4ac5a955f4ba4fe93fc8cef27558a3eb9d2a529a2092761fb833b656cd48b9de6a
 
-keytool -list -v  -keystore /Users/sp/Codes/Camps/campy_secret/upload-keystore.jks \  
-    -alias CampyAndroidKey
+keytool -list -v  -keystore /Users/sp/Codes/Camps/campy_secret/upload-keystore.jks -alias CampyAndroidKey
