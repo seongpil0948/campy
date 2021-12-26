b7785a9b586887aafd9b4f921c46de2df0c5ece8
seongpil0948 <seongpil0948@gmail.com>
2021-12-26 22:36:45 +0900

    update/1.0.2

--- scripts/ios_build.sh
+++ scripts/ios_build.sh
@@ -1,4 +1,7 @@
 flutter clean \
-    && rm ios/Podfile.lock pubspec.lock \
-    && rm -rf ios/Pods ios/Runner.xcworkspace \
-    && flutter build ipa  
+    # && rm ios/Podfile.lock pubspec.lock \
+    # && rm -rf ios/Pods ios/Runner.xcworkspace \
+
+flutter build ipa  
+
+open -a Xcode /Users/sp/Codes/Camps/campy/build/ios/archive/Runner.xcarchive
